dafe18f171b302e3d56b09465f2c8c8abade08f6
Austin Guiney <austin.guiney@gmail.com>
2025-11-07 22:20:17 -0800

    Renamed DB_HOST to SERVER_HOST.

--- server/models/db.js
+++ server/models/db.js
@@ -21,7 +21,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../', '.env') });
  */
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: process.env.SERVER_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
