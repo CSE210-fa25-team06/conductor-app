6256337bc5e7b6431804bfb476e418fe29fdfccd
Akshay Kamath <akkamath@ucsc.edu>
2025-11-09 17:57:32 -0800

    adding support for reading from environment variables

--- server/models/db.js
+++ server/models/db.js
@@ -2,7 +2,7 @@
   This file establishes a connection to the PostgreSQL database using the pg library.
   It exports a connection pool used by other model files for database operations.
 */
-
+import dotenv from "dotenv";
 import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config()
