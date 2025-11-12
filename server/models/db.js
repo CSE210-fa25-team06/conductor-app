05f71cedae82e24e1007c402a24f551b9ac2f44f
Akshay Kamath <akkamath@ucsc.edu>
2025-11-12 12:53:05 -0800

    proper env reading

--- server/models/db.js
+++ server/models/db.js
@@ -2,7 +2,10 @@
   This file establishes a connection to the PostgreSQL database using the pg library.
   It exports a connection pool used by other model files for database operations.
 */
+
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config()
 const { Pool } = pkg;
 
 export const pool = new Pool({
