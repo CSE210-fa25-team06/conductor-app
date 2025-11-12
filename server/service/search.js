779ff4afab1754981c5b51b65d7fa558d9bdabbb
Akshay Kamath <akkamath@ucsc.edu>
2025-11-11 16:02:12 -0800

    minor fix to have right name for roles field

--- server/service/search.js
+++ server/service/search.js
@@ -8,7 +8,7 @@ import { pool } from "../models/db.js";
 
 export async function searchDirectory(query) {
     let baseQuery = `
-        SELECT u.id, u.photo_url, u.name, u.email, array_agg(r.name), g.name as group_name, contact_info, availability
+        SELECT u.id, u.photo_url, u.name, u.email, array_agg(r.name) as roles, g.name as group_name, contact_info, availability
         FROM users u
         LEFT JOIN user_roles ur ON u.id = ur.user_id
         LEFT JOIN roles r on ur.role_id = r.id
