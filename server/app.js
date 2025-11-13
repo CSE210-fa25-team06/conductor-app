9ecc744a478ad8c668950bb352334888475ef7f8
Patryk Tomlak <patryktomalak.email@gmail.com>
2025-11-13 12:03:05 -0800

    Serve frontend files from Express
    Added a line to `server/app.js` to serve the static files from our `/client` directory.

--- server/app.js
+++ server/app.js
@@ -32,6 +32,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
 /**
  * Express-Session Middleware Configuration.
  */
