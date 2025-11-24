d0c9bb67aabf0de854bb7bafcd3a4cf5c3f0041a
Akshay Kamath <akkamath@ucsc.edu>
2025-11-24 11:02:59 -0800

    adding documentation and removing some unneeded awaits

--- tests/integration/client/directory.test.js
+++ tests/integration/client/directory.test.js
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+/*
+This file contains a test suite of integration tests for the class directory screen.
+It checks for existence of main elements sucha s the search bar and title. It checks functionality of searching as well.
+*/
+
+// Only professors can use the class directory feature so a professor account needs to be logged in
 test.beforeAll(() => {
   process.env.MOCK_EMAIL = "ian@example.com";
 });
 
+// reset to original logged in user because dashboard test depends on it
 test.afterAll(() => {
   process.env.MOCK_EMAIL = "alice@example.com";
 });
@@ -13,16 +20,24 @@ test.beforeEach(async ({ page }) => {
     const loginButton = page.locator('a#google-login');
     loginButton.click();
     await page.getByTestId('directory-tab').click();
+
+    /* 
+    the reason this needs to be awaited is that the new html renders after a 250 ms delay.
+    This creates issues with consistent testing across different browsers.
+    A solution here is to await the class directory title so every test starts when the screen is loaded.
+    If this were to hypothetically fail, it would not trigger an infinite loop. Playwright would stop it after 5 seconds.
+    */
     await expect(page.getByText('Class Directory')).toBeVisible();
 });
 
 
+//same check as in the beforeEach but explicitly laid out here for clarity
 test('Class Directory title appears', async ({ page }) => {
-    await expect(page.getByText('Class Directory')).toBeVisible();
+    expect(page.getByText('Class Directory')).toBeVisible();
 });
 
 test('Search Bar Appears', async ({ page }) => {
-    await expect(page.locator('.directory input')).toBeVisible();
+    expect(page.locator('.directory input')).toBeVisible();
 });
 
 test('Search bar properly retrieves names that match via the prefix', async ({ page }) => {
