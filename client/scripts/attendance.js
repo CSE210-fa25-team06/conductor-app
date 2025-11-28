b6a951a2d7eeb4c85785f4083cc7e8decc253694
Jared Ziv <jaredaziv@gmail.com>
2025-11-27 17:50:31 -0800

    Preliminary QR code JS logic and html/css update

--- client/scripts/attendance.js
+++ client/scripts/attendance.js
@@ -16,6 +16,58 @@ export function renderAttendance(containerEl) {
     });
 }
 
+function startMeeting() {
+	const qrModal = createQRModal();
+	initQRModal(qrModal);
+}
+
+function createQRModal() {
+	//find QR code template
+	const qrTemplate = document.getElementById("QRModal");
+	const qrModalContent = qrTemplate.content;
+	//have to make a deep copy of the content
+	const qrModal = document.importNode(qrModalContent, true)
+
+	//add element to DOM
+	document.body.appendChild(qrModal);
+
+	//return reference to modal
+	const modalRoot = document.getElementById("modalRoot");
+	modalRoot.classList.add("active")
+	return modalRoot
+}
+
+function initQRModal(qrModal) {
+	//initialize content of qrModal. needs to:
+	//1. update QR code image to relevant meeting QR
+	//2. create end meeting button listener to end current meeting
+	//3. set up close button functionality to remove element from the DOM
+
+	//1. update QR image
+	const img = document.getElementById("qrImage");
+	img.src = "TEMP-QR-TEST.png" //PUT QR IMAGE or whatever the endpoint generates here
+
+	//2. end meeting event listener
+	const endMeetingBtn = document.getElementById("endMeeting");
+	endMeetingBtn.addEventListener("click", endMeeting);
+
+	//3. close button event listener
+	const removeModal = () => {
+		qrModal.classList.remove('active');
+		setTimeout(() => {qrModal.remove()}, 300);
+	}
+	const closeBtn = document.getElementById("closeModal");
+	closeBtn.addEventListener("click", removeModal);
+}
+
+function endMeeting(meetingID) {
+	//end meeting - need to ensure user is authorized to end
+	//the meeting they are trying to end
+
+	//TODO: connect with backend
+	console.log(`Meeting ${meetingID} ended.`);
+}
+
 function initAttendanceLogic() {
 	// Toggle student selection
 	const studentRows = document.querySelectorAll('.student-row')
@@ -24,6 +76,10 @@ function initAttendanceLogic() {
 			row.classList.toggle('selected')
 		})
 	})
+
+	// Start meeting button
+	const meetingBtn = document.getElementById("startMeetingBtn");
+	meetingBtn.addEventListener("click", startMeeting)
 
 	// Date input formatting
 	const dateInput = document.getElementById('meetingDate')
