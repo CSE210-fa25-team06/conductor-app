a3a2f1dc1f2c1f18a37440dbcf0e958d02bff06c
Adam DEZIRI <adamdeziri@proton.me>
2025-12-08 21:32:17 -0800

    feat: improve perf with index on users/date while retrieving attendance

--- db/schema.sql
+++ db/schema.sql
@@ -156,6 +156,14 @@ CREATE TABLE attendance (
     UNIQUE (user_id, session_id)
 );
 
+CREATE INDEX idx_attendance_user_id ON attendance(user_id)
+
+-- Optimizes SQL queries for retrieving attendance based on the associated user_id (very common condition used by all user roles)
+
+CREATE INDEX idx_attendance_date ON attendance(date)
+
+-- An index based on the date, because other users retrieve attendance by date mostly
+
 -- Table: journals
 -- Stores weekly student journal submissions.
 CREATE TABLE journals (
