4f11b049f2eeb8756efc9e3ae8b8b73540085699
Melvyn <melvyn9@gmail.com>
2025-12-09 17:56:46 -0800

    Calendar integrated

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -14,20 +14,7 @@ module.exports = {
   It provides functions for managing group records and relationships.
 */
 
-// const { pool } = require("./db");
-
-// async function getAllGroups() {
-//   const result = await pool.query(
-//     "SELECT id, name, created_at FROM groups ORDER BY name"
-//   );
-//   return result.rows;
-// }
-
-// module.exports = {
-//   getAllGroups
-// };
-
-import { pool } from "./db.js";
+// import { pool } from "./db.js";
 
 // export async function getAllGroups() {
 //   const result = await pool.query("SELECT * FROM groups");
